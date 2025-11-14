7dcaaf0214b190e39319f13c632b6cf51c20aa62
Felipe <felipemonzon0710@gmail.com>
2025-11-14 09:56:46 -0300

    creación de un hook personalizado para useForm

---
+++ src/hooks/useForm.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+const useForm = (initialValues = {}) => {
+  const [values, setValues] = useState(initialValues);
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    const newValue = type === "checkbox" ? checked : value;
+    setValues((prevValues) => ({ ...prevValues, [name]: newValue }));
+    setValues({ ...values, [name]: newValue });
+  };
+
+  const handleReset = () => {
+    setValues(initialValues);
+  };
+
+  return { values, setValues, handleChange, handleReset };
+};
+
+export default useForm;
